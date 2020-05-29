1b0a2ea93f543c8815bf8f553a86d583403075d0
traineryou <64368236+traineryou@users.noreply.github.com>
2020-05-29 22:52:11 +0300

    Update remocolab.py

--- remocolab.py
+++ remocolab.py
@@ -225,7 +225,7 @@ def _setupVNC():
   apt.debfile.DebPackage("virtualgl.deb", cache).install()
   apt.debfile.DebPackage("turbovnc.deb", cache).install()
 
-  _installPkgs(cache, "xfce4", "xfce4-terminal" , "xfce4-goodies", "firefox", "qbittorrent", "filezilla" )
+  _installPkgs(cache, "xfce4", "xfce4-terminal" , "xfce4-goodies", "firefox", "qbittorrent", "filezilla")
   cache.commit()
   
   vnc_sec_conf_p = pathlib.Path("/etc/turbovncserver-security.conf")
