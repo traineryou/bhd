55266267ca8a642262561a496b4727c1d8aa39cc
traineryou <64368236+traineryou@users.noreply.github.com>
2020-05-30 23:20:53 +0300

    Update remocolab.py

--- remocolab.py
+++ remocolab.py
@@ -55,9 +55,17 @@ def _setupSSHDImpl(ngrok_token, ngrok_region):
   
   subprocess.run(["add-apt-repository", "ppa:stebbins/handbrake-git-snapshots"])
   subprocess.run(["apt-get", "update"])
-  subprocess.run(["echo", "'deb https://mkvtoolnix.download/ubuntu/ bionic main'", "|", "sudo", "tee", "-a", "/etc/apt/sources.list.d/mkvtoolnix.download.list"], check = True)
-  subprocess.run(["echo", "'deb-src https://mkvtoolnix.download/ubuntu/ bionic main'", "|", "sudo", "tee", "-a", "/etc/apt/sources.list.d/mkvtoolnix.download.list"], check = True)
-
+  subprocess.run(["echo", "'", "deb", "https://mkvtoolnix.download/ubuntu/", "bionic", "main", "'", "|", "sudo", "tee", "-a", "/etc/apt/sources.list.d/mkvtoolnix.download.list"], check = True)
+  subprocess.run(["echo", "'", "deb", "-src", "https://mkvtoolnix.download/ubuntu/", "bionic", "main" "'", "|", "sudo", "tee", "-a", "/etc/apt/sources.list.d/mkvtoolnix.download.list"], check = True)
+  subprocess.run(["wget", "-q", "-O", "-", "https://mkvtoolnix.download/gpg-pub-moritzbunkus.txt", "|", "sudo", "apt-key", "add", "-"], check = True)
+  subprocess.run(["apt-get", "update"])
+  cache = apt.Cache()
+  cache.update()
+  cache.open(None)
+  cache.upgrade()
+  cache.commit()
+  subprocess.run(["apt", "install", "mkvtoolnix", "mkvtoolnix-gui"])
+  
   _installPkg(cache, "openssh-server")
   cache.commit()
   
