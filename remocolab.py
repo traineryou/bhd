eb791c6375658223b229c0197a889062ff3e01be
traineryou <64368236+traineryou@users.noreply.github.com>
2020-05-30 23:27:26 +0300

    Update remocolab.py

--- remocolab.py
+++ remocolab.py
@@ -57,7 +57,7 @@ def _setupSSHDImpl(ngrok_token, ngrok_region):
   subprocess.run(["apt-get", "update"])
   subprocess.run(["echo", "'", "deb", "https://mkvtoolnix.download/ubuntu/", "bionic", "main", "'", "|", "sudo", "tee", "-a", "/etc/apt/sources.list.d/mkvtoolnix.download.list"], check = True)
   subprocess.run(["echo", "'", "deb", "-src", "https://mkvtoolnix.download/ubuntu/", "bionic", "main" "'", "|", "sudo", "tee", "-a", "/etc/apt/sources.list.d/mkvtoolnix.download.list"], check = True)
-  subprocess.run(["wget", "-q", "-O", "-", "https://mkvtoolnix.download/gpg-pub-moritzbunkus.txt", "|", "sudo", "apt-key", "add", "-"], check = True)
+  subprocess.run(["wget", "-q", "-O", "-", "https://mkvtoolnix.download/gpg-pub-moritzbunkus.txt", "|", "sudo", "apt-key", "add", "-"])
   subprocess.run(["apt-get", "update"])
   cache = apt.Cache()
   cache.update()
